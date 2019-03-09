b9a5ef555713806235aba43fc17f0d8fb3693a9f
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2019-03-09 09:41:38 -0300

    Config elixirc_paths to load test/support directory

--- mix.exs
+++ mix.exs
@@ -5,8 +5,9 @@ defmodule TriplexPlug.MixProject do
     [
       app: :triplex_plug,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
     ]
   end
@@ -18,11 +19,14 @@ defmodule TriplexPlug.MixProject do
     ]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:plug, "~> 1.7"}
     ]
   end
 end
